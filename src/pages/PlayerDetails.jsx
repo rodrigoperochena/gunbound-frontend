e1d0e7f2992268116bc20d2fb55c06bf87c513fa
tak <taksumaq@ubuntu.server>
2025-01-05 03:12:46 +0800

    Enhanced PlayerDetails page UI and loading experience
    - Replaced text-based loading message with animated Raon GIF for better user feedback.
    - Added detailed player profile header with user-specific welcome message.
    - Split user details into 'Overview' and 'Game Stats' sections for better readability.
    - Improved display of game stats with consistent font and styling.
    - Updated rank and country displays with titles for better accessibility.

--- src/pages/PlayerDetails.jsx
+++ src/pages/PlayerDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'; // For dynamic routing
 import { format } from 'date-fns';
 import usePageBodyClass from '../hooks/usePageBodyClass';
 import Flag from 'react-flagpack';
+import raon from '../assets/images/raon-vehicle.gif';
 
 const getGradeImage = (grade) => `/assets/grades/${grade}.png`;
 const rankNames = {
@@ -48,37 +49,60 @@ const PlayerDetails = () => {
     fetchUserDetails();
   }, [id]); // Runs whenever the 'id' in the URL changes
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  if (loading) return (
+    <div className="container">
+      <img src={raon} alt="raon loading" />
+    </div>
+  )
+  if (error) return (
+    <div className="container">
+      <p>Error: {error}</p>
+    </div>
+  )
+  
 
   return (
     <div className="user-details">
       <div className='container'>
-        <h3 className='heading'>User Details📝</h3>
-        <ul className="user-details-list">
-          <li className='user'>
-            User: {userDetails.userId}
-          </li>
-          <li className='rank'>Rank:
-            <img
-              src={getGradeImage(userDetails.totalGrade)}
-              alt={`Grade ${userDetails.totalGrade}`}
-            />
-            {rankNames[userDetails.totalGrade]}
+        <div className="main-header">
+          <h3 className='heading'>🎮 {userDetails.userId}</h3>
+          <p>Welcome to <span>{userDetails.userId}'s</span> profile. Find their rank, game points, and more&nbsp;below.</p>
+        </div>
+        <div className="player-overview">
+          <h4 className="heading">🕹 Overview</h4>
+          <ul className="user-details-list">
+            <li className='user' title='User ID'>
+              Username: {userDetails.userId}
             </li>
-          <li>GP: {userDetails.totalScore}</li>
-          <li>Gold: <span className="gold">{userDetails.money}</span></li>
-          <li>Cash: <span className="cash">{userDetails.cash}</span></li>
-          <li>Country: 
-            <Flag 
-              code={countryFlags[userDetails.country]} 
-              size="m"
-              hasBorderRadius
-            />
-            {userDetails.country}
-          </li>
-          <li>Last Login: {' '}{format(new Date(userDetails.lastLoginTime), 'h:mm aaa, MMM do yyyy')}</li>
+            <li className='rank' title='Game Rank'>Rank: 
+              <img
+                src={getGradeImage(userDetails.totalGrade)}
+                alt={`Grade ${userDetails.totalGrade}`}
+              />
+              {rankNames[userDetails.totalGrade]}
+              </li>
+            <li title='Country'>Country:
+              <Flag
+                code={countryFlags[userDetails.country]}
+                size="m"
+                hasBorderRadius
+              />
+              {userDetails.country}
+            </li>
+            <li title='Last Login Time'>Last Online: {' '}{format(new Date(userDetails.lastLoginTime), 'h:mm aaa, MMM do yyyy')}</li>
+          </ul>
+        </div>
+
+        <div className="game-stats">
+          <h4 className='heading'>📊 Game Stats</h4>
+          <ul className="user-details-list">
+          <li className='gp' title='Game Points'>GP: <span className="mono-font">{userDetails.totalScore}</span></li>
+          <li title='win-rate'>Win Rate:<span className="mono-font">{userDetails.winRate}%</span></li>
+          <li title='Gold'>Gold: <span className="mono-font gold">{userDetails.money}</span></li>
+          <li title='Cash'>Cash: <span className="mono-font cash">{userDetails.cash}</span></li>
         </ul>
+
+        </div>
       </div>
     </div>
   );
