e953844f36e02f245dc33e47aa2f6d9c9dcf061c
tak <taksumaq@ubuntu.server>
2025-01-05 03:22:06 +0800

    Refactored Header component for SPA-friendly navigation
    - Replaced <a> tag with <Link> component for internal navigation to the Home page.
    - Ensured consistent use of React Router links to prevent full page reloads.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 // frontend/src/components/Header.jsx
 import React from 'react';
+import { Link } from "react-router-dom";
 import logo from '../assets/images/raon-vehicle.png';
 import Navigation from './Navigation';
 
@@ -7,13 +8,13 @@ const Header = () => {
   return (
     <header className='site-header'>
       <div className="container">
-        <a href="/" className="logo">
+        <Link to='/' className='logo'>
           <img src={logo} alt="Raon launcher" />
           <div className="logo-text">
             <h1 className='site-title'>Gunbound</h1>
             <h2 className="site-subtitle">Thor's Hammer 🚀</h2>
           </div>
-        </a>
+        </Link>
         <Navigation />
       </div>
     </header>
