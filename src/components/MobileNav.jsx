e0411fb7c1e5daf8489e10cbd8c53cbcbfa0974b
tak <taksumaq@ubuntu.server>
2025-01-05 03:19:16 +0800

    Updated MobileNav links for improved navigation
    - Replaced 'Players' link with 'How to join' for a more intuitive option.
    - Added a direct link to 'Player Rankings' for quicker access.

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -11,10 +11,13 @@ const MobileNav = ({ isOpen }) => {
           <Link to='/' className='button'>Home</Link>
         </li>
         <li className="menu-item">
-          <Link to='/users' className='button'>Players</Link>
+          <Link to='/how-to-join' className='button'>How to join</Link>
         </li>
         <li className="menu-item">
           <Link to='/register' className='button'>Register</Link>
+        </li>
+        <li className="menu-item">
+          <Link to='/player-rankings' className='button'>Player Rankings</Link>
         </li>
       </ul>
     </nav>
